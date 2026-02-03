1964e05ae46995ccdf12184b5c206c8d0c83964a
Keller.Benedikt-25 <Keller.Benedikt-25@host.docker.internal>
2026-02-03 13:02:29 +0100

    Dekleration und Innitialisierung

---
+++ src/main/java/main/D04_DataObjects.java
@@ -0,0 +1,25 @@
+package main;
+
+public class D04_DataObjects {
+	// Konstante
+	public final static double PI = 3.1415;
+	public static void main(String[] args) {
+		// Dekleration
+		int age;
+		String name, firstName, famiyName, firstAndFamilyName;
+		double sizeInM;
+		boolean isMale;
+		// innitialisierung
+		age=18;
+		firstName="Ben";
+		sizeInM = 1.8;
+		isMale = true;
+		
+		//Ausgabe
+		
+	System.out.println("age: "+ age);
+	System.out.println("firstname:"+ firstName);
+	System.out.println("PI:" + PI);
+	}
+ 
+}
